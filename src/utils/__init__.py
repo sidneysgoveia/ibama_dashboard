673d9b7c98feeee8d95d6b9329ba49e0635adf81
Reinaldo Chaves <reichaves@gmail.com>
2025-07-30 16:13:44 -0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,16 @@
-# IBAMA Data Analysis - Utils package 
+# Este conteúdo deve ser copiado para cada arquivo __init__.py
+
+# src/__init__.py
+"""
+Módulo principal do projeto IBAMA Dashboard.
+"""
+
+# src/components/__init__.py
+"""
+Componentes de interface do usuário.
+"""
+
+# src/utils/__init__.py
+"""
+Utilitários e funções auxiliares.
+"""
