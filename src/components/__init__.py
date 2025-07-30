bf6142f7f0d9bc06221c7268bf793ff1ff6455c9
Reinaldo Chaves <reichaves@gmail.com>
2025-07-30 16:15:20 -0300

    Update __init__.py

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,16 +1,4 @@
-# Este conteúdo deve ser copiado para cada arquivo __init__.py
-
-# src/__init__.py
-"""
-Módulo principal do projeto IBAMA Dashboard.
-"""
-
 # src/components/__init__.py
 """
 Componentes de interface do usuário.
-"""
-
-# src/utils/__init__.py
-"""
-Utilitários e funções auxiliares.
 """
